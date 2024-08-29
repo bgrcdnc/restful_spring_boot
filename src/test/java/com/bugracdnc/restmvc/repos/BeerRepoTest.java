dac8ec1d25b69b04b5f3024dcf5c1c412b48181f
bgrcdnc <bugracaliskandinc@gmail.com>
2024-08-29 23:56:56 +0300

    Refactor: Now conforms to Validation

--- src/test/java/com/bugracdnc/restmvc/repos/BeerRepoTest.java
+++ src/test/java/com/bugracdnc/restmvc/repos/BeerRepoTest.java
@@ -1,11 +1,16 @@
 package com.bugracdnc.restmvc.repos;
 
 import com.bugracdnc.restmvc.entities.Beer;
+import com.bugracdnc.restmvc.models.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepoTest {
@@ -13,10 +18,29 @@ class BeerRepoTest {
     BeerRepo beerRepo;
 
     @Test
+    void testSaveBeerTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepo.save(Beer.builder()
+                                               .beerName("Test Beer8917239812739128739128739182739817239812739812739812739817293871293871298371298379128379218")
+                                               .beerStyle(BeerStyle.PILSNER)
+                                               .upc("123456")
+                                               .price(new BigDecimal("25.5"))
+                                               .build());
+
+            beerRepo.flush();
+        });
+    }
+
+    @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepo.save(Beer.builder()
                                            .beerName("Test Beer")
+                                           .beerStyle(BeerStyle.PILSNER)
+                                           .upc("123456")
+                                           .price(new BigDecimal("25.5"))
                                            .build());
+
+        beerRepo.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
