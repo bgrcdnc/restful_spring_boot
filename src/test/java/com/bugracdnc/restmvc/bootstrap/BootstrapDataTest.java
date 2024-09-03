8093a93513f612a9d35b1d2f1479fbc7f0d4df0f
bugracdnc <bugracaliskandinc@gmail.com>
2024-09-03 22:58:48 +0300

    Update tests to account for csv values

--- src/test/java/com/bugracdnc/restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/bugracdnc/restmvc/bootstrap/BootstrapDataTest.java
@@ -3,14 +3,17 @@ package com.bugracdnc.restmvc.bootstrap;
 import com.bugracdnc.restmvc.repos.BeerRepo;
 import com.bugracdnc.restmvc.repos.CustomerRepo;
 import com.bugracdnc.restmvc.services.BeerCsvService;
+import com.bugracdnc.restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -33,7 +36,7 @@ class BootstrapDataTest {
     void testRun() throws Exception {
         bootstrapData.run((String) null);
 
-        assertThat(beerRepo.count()).isEqualTo(3);
+        assertThat(beerRepo.count()).isEqualTo(2410);
         assertThat(customerRepo.count()).isEqualTo(3);
     }
 }
